028b26be05a478c91690201c1bbd28ed459889d9
tjvonbr <51134664+tjvonbr@users.noreply.github.com>
2019-07-22 14:45:47 -0500

    Finished MVP for today's first project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { Home, About, Contact, Navigation } from './components';
+import { Route, Switch } from 'react-router-dom';
 
 const App = () => (
-  <div>
-    <Navigation />
-  </div>
+  <Router>
+    <div>
+      <Navigation />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" exact component={Contact} />
+      </Switch>
+    </div>
+  </Router>
 );
 
 export default App;
